5de3689e71c875655f347aa06498103c83170d0e
Filippo Benozzi <email@filippo.im>
2025-01-29 17:50:35 +0100

    update update.sh

--- update.sh
+++ update.sh
@@ -7,24 +7,27 @@ APP_DIR=~/filippo.im
 # Pull the latest changes from the Git repository
 if [ -d "$APP_DIR" ]; then
   echo "Pulling latest changes from the repository..."
-  cd $APP_DIR
+  cd "$APP_DIR"
   git pull origin main
 else
   echo "Cloning repository from $REPO_URL..."
-  git clone $REPO_URL $APP_DIR
-  cd $APP_DIR
+  git clone "$REPO_URL" "$APP_DIR"
+  cd "$APP_DIR"
 fi
 
-# Build and restart the Docker containers from the app directory (~/myapp)
-echo "Rebuilding and restarting Docker containers..."
-docker compose down
+# Ricostruisci e avvia i container
+echo "Building and starting Docker containers..."
 docker compose up --build -d
 
-# Check if Docker Compose started correctly
-if ! docker compose ps | grep "Up"; then
-  echo "Docker containers failed to start. Check logs with 'docker-compose logs'."
+# Verifica che i container siano effettivamente in esecuzione
+if ! docker compose ps | grep -q "Up"; then
+  echo "Docker containers failed to start. Check logs with 'docker compose logs'."
   exit 1
 fi
 
-# Output final message
+# Elimina tutte le immagini non utilizzate
+echo "Removing unused Docker images..."
+docker image prune -af
+
+# Output finale
 echo "Update complete. Your Next.js app has been deployed with the latest changes."
